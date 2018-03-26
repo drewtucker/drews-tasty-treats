550c293887f8b5d04f7fd0d52bc5723567d8a6cc
Guest User <Guest@epicodus-6.local>
2018-03-26 13:35:49 -0700

    Finish the tasty treats, add script to hide userName form after entry.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
+import 'bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = "Drew's Tasty Treats";
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+  recipes: Recipe[] = [
+  new Recipe("Drew's Delicious Dumplings", ["1. Buy dumplings", "2. Cook dumplings"], 2),
+  new Recipe("Drew's Doughy Doughnuts", ["1. Make the doughnuts", "2. Don't NOT make the doughnuts"], 1),
+  new Recipe("Drew's Delicious Mystery Sauce", ["1. ???", "2. Eat the sauce"], 3)
+];
+
+deliciousnessColor(currentRecipe)
+{
+  if(currentRecipe.deliciousness === 3)
+  {
+    return "bg-danger";
+  }
+  else if(currentRecipe.deliciousness === 2)
+  {
+    return "bg-warning";
+  }
+  else
+  {
+    return "bg-info";
+  }
+}
+
+selectedRecipe = null;
+enteredName = true;
+
+editRecipe(clickedRecipe)
+{
+  this.selectedRecipe = clickedRecipe;
+}
+
+finishedEditing()
+{
+  this.selectedRecipe = null;
+}
+
+finishedName()
+{
+  this.enteredName = null;
+}
+
+changeName()
+{
+  this.enteredName = true;
+}
+
+
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 if (environment.production) {
   enableProdMode();
